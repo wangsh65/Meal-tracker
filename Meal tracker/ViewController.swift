6df70df4709998201afc7e7f086eb48a92448713
wangsh65 <wangsh@ssfs.org>
2024-04-25 11:25:36 -0400

    Initial Commit

---
+++ Meal tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal tracker
+//
+//  Created by Shelly on 4/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
